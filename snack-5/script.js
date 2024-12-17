3a4f2816ca43af3cf456b66ed3f3fe3bb3e4590f
MirkoDD <mirkodidio111@gmail.com>
2024-12-17 16:47:32 +0100

    added:snack_5

---
+++ snack-5/script.js
@@ -0,0 +1,7 @@
+const nums = [2, 8, 4, 7, 12, 87];
+
+// Crea un array a partire dall'array nums, che abbia solo i numeri pari
+// Risultato: [2, 8, 4, 12]
+
+const filtered = nums.filter((filter) => !(filter % 2))
+console.log(filtered)
